5d991650199af342bc177c540d042c144ee81af9
kababukingdom <kababukingdom@gmail.com>
2023-04-25 18:36:06 +0300

    hello-world

---
+++ hello-world/tests/hello_world_test.rs
@@ -0,0 +1,9 @@
+#[test]
+
+
+#[test]
+fn hello_test() {
+    let sys = System::new();
+    sys.init_logger();
+    let program = Program::current(&sys);
+    program.send_bytes(2, String::from("INIT MESSAGE"));
